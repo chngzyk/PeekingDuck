8bb5b34bc77a1264a1b5d8e4220f5b2622d5ccc1
chngzyk <chngzyk@gmail.com>
2021-10-26 15:17:33 +0800

    test

--- tests/pipeline/nodes/model/yolov4_license_plate/test_yolo_license_plate.py
+++ tests/pipeline/nodes/model/yolov4_license_plate/test_yolo_license_plate.py
@@ -46,8 +46,8 @@ def replace_download_weights(root, blob_file):
 @pytest.mark.mlmodel
 class TestLPYolo:
     def test_no_LP_image(self, test_no_lp_images, LPyolo):
-        blank_image = cv2.imread(test_no_lp_images)
-        # output = LPyolo.run({"img": blank_image})
+        # blank_image = cv2.imread(test_no_lp_images)
+        # # output = LPyolo.run({"img": blank_image})
 
         assert 0 == 0
 
